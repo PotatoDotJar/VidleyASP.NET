6ff4506b8b8f3770cdb00f07d728fe363a1149a0
RJ Nader <rj@potatosaucevfx.com>
2018-12-02 22:22:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+VidleyASP.NET
---
+++ Vidley/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+
+namespace Vidley.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Title = "The Bee Movie" };
+
+            return View(movie);
+        }
+    }
+}
