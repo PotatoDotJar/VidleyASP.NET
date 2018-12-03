f3fafe98c8166f7cff7ecee5a0c3cf61df759806
RJ Nader <rj@potatosaucevfx.com>
2018-12-03 01:10:29 -0500

    Adding more to controllers.

--- Vidley/Controllers/MovieController.cs
+++ Vidley/Controllers/MovieController.cs
@@ -16,5 +16,33 @@ namespace Vidley.Controllers
 
             return View(movie);
         }
+
+        // GET: Movie/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            return Content("ID=" + id);
+        }
+
+        public ActionResult Index(int? pageNumber, string sortBy)
+        {
+            if(!pageNumber.HasValue)
+            {
+                pageNumber = 1;
+            }
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageNumber={0}&sortBy={1}", pageNumber, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("{0}/{1}", year, month));
+        }
     }
 }
+
+//https://youtu.be/E7Voso411Vs?t=2439
